f3755daed09330f0c5ad22e0258ed36321cd0b4d
Leonardus Chen <leonardus.chen@gmail.com>
2021-03-31 20:01:30 +0800

    Release v0.0.2 (#6)
    * Update README.md to include installation and quickstart
    * Bump to v0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pysloth",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(exclude=["etc", "tests", "docs"]),
     description="Probabilistic Predictions",
     long_description=long_description,
