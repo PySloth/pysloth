8df5ada12fe12d1e3ac53198dda8630e08f01601
Leonardus <leonardus.chen@gmail.com>
2021-04-02 12:27:13 +0800

    Add images for logo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -54,10 +54,11 @@ html_theme = 'pydata_sphinx_theme'
 
 html_theme_options = {
     "github_url": "https://github.com/PySloth/pysloth",
-    "show_prev_next": False
+    "show_prev_next": False,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = "_static/blue_pysloth_logo.png"
