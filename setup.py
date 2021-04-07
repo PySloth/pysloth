f7e9cbae912202a1ed2676609d114758e838f854
Leonardus Chen <leonardus.chen@gmail.com>
2021-04-07 23:29:49 +0800

    Release v0.0.3 (#14)
    - Rename coveragerc typo, omit irrelevant files from coverage
    - Bump to v0.0.3

--- setup.py
+++ setup.py
@@ -5,14 +5,17 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setup(
     name="pysloth",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(exclude=["etc", "tests", "docs"]),
+
     description="Probabilistic Predictions",
     long_description=long_description,
     long_description_content_type="text/x-rst",
+
     url="https://github.com/PySloth/pysloth",
     author="PySloth",
     author_email="pysloth.python@gmail.com",
+
     license="Apache Software License",
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -27,6 +30,7 @@ setup(
         "scikit-learn>=0.24.1",
         "numpy>=1.19.2",
     ],
+
     python_requires=">=3.6",
     tests_require=["pytest"],
     setup_requires=['pytest-runner']
